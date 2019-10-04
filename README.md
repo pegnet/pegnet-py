d0f8fd006a08fcffa63db9ebae156f8c23c56451
Sam Barnes <mistersamuelbarnes@gmail.com>
2019-10-04 10:42:46 -0500

    Add README LICENSE and setup.py

---
+++ README.md
@@ -0,0 +1,12 @@
+# fat-py
+
+Kinda ugly, very alpha.
+
+## Usage
+
+```
+import fat_py
+fatd = fat_py.FATd()
+fatd.get_sync_status()
+{'syncheight': 62, 'factomheight': 62}
+```
---
+++ setup.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+
+
+try:
+    import pypandoc
+    long_description = pypandoc.convert("README.md", "rst")
+except (IOError, ImportError, OSError):
+    long_description = open("README.md").read()
+
+
+CLASSIFIERS = [
+    "Development Status :: 4 - Beta",
+    "Intended Audience :: Developers",
+    "License :: OSI Approved :: MIT License",
+    "Operating System :: OS Independent",
+    "Programming Language :: Python :: 3.5",
+    "Programming Language :: Python :: 3.6",
+    "Programming Language :: Python :: 3.7",
+    "Topic :: Internet :: WWW/HTTP",
+    "Topic :: Security",
+    "Topic :: Security :: Cryptography",
+    "Topic :: Software Development",
+    "Topic :: System :: Monitoring",
+]
+
+
+setup(
+    name="fat-py",
+    version="0.0.1",
+    description="Python client library for the FATd API",
+    long_description=long_description,
+    license="MIT License",
+    platforms=["OS Independent"],
+    classifiers=CLASSIFIERS,
+    packages=find_packages(exclude=["tests"]),
+    include_package_data=True,
+    install_requires=[
+        "requests>=2.20.0",
+    ],
+    url="https://github.com/pegnet/fat-py",
+)
